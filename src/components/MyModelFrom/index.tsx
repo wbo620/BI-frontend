9ce293184271f11c489c816dfc09a0ae045bf8c6
wbo620 <468308027@qq.com>
2023-11-19 22:30:24 +0800

    封装删除，详情页按钮

---
+++ src/components/MyModelFrom/index.tsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import {Button, message, Modal} from 'antd';
+import {ModalForm} from "@ant-design/pro-form";
+import MyCard from "@/components/Mychart";
+import {deleteChartUsingPOST} from "@/services/icebi/chartController";
+// 将详情，删除按钮封装
+const MyModelFrom = ({item, onChartDeleted}) => {
+
+
+  // 删除图表
+  const handleDelete = async (id: number) => {
+    try {
+      const res = await deleteChartUsingPOST({id});
+      if (res.code !== 0) {
+        message.error('删除失败')
+      }
+      // 调用回调函数刷新 MyChartPage 中的数据
+      onChartDeleted();
+      message.success('删除成功');
+
+    } catch (error) {
+      console.error('删除图表时出错:', error);
+      message.error('删除失败');
+    }
+  };
+
+  return (
+    <div style={{textAlign: 'right'}}>
+      <Button danger key="delete"
+              style={{marginLeft: '10px', marginRight: '10px', bottom: '1'}}
+              onClick={() => {
+                Modal.confirm({
+                  title: '确认删除',
+                  content: '确定要删除这个图表吗？',
+                  onOk: () => handleDelete(item.id ?? 0), // Convert id to a number
+                });
+              }}>
+        删除
+      </Button>
+      <ModalForm
+        title="详情"
+        trigger={<Button type="primary">详情</Button>}
+        width="80%"
+        submitter={{
+          render: () => {
+            return [
+              <Button danger key="delete" onClick={() => {
+                Modal.confirm({
+                  title: '确认删除',
+                  content: '确定要删除这个图表吗？',
+                  onOk: () => handleDelete(item.id ?? 0), // Convert id to a number
+                });
+              }}>
+                删除
+              </Button>,
+            ];
+          }
+        }}
+        onFinish={async (values) => {
+          console.log(values);
+          message.success('删除成功');
+          return true;
+        }}
+      >
+        <div style={{marginBottom: 16}}/>
+        <div>{'图表标题: ' + item.name}</div>
+        <div>{'图表ID: ' + item.id}</div>
+        <div>{'创建时间: ' + new Date(item.createTime).toLocaleString('zh-CN', {
+          year: 'numeric',
+          month: '2-digit',
+          day: '2-digit',
+          hour: '2-digit',
+          minute: '2-digit',
+          second: '2-digit',
+        })}</div>
+        <div>{'分析目标：' + item.goal}</div>
+        <div style={{marginBottom: 16}}/>
+        {/*修复bug 把ReactECharts放入Card标签中*/}
+        {/*出现bug的原因可能是，option的加载比div的加载速度快，导致第一次加载没有挂在到div里，再次点开，才被挂载到*/}
+        {item.genChart && <MyCard genChart={item.genChart}/>}
+        <div>{item.genResult ?? ''}</div>
+      </ModalForm>
+    </div>
+  );
+};
+
+export default MyModelFrom;
--- src/pages/MyChart/index.tsx
+++ src/pages/MyChart/index.tsx
@@ -1,11 +1,9 @@
-
-import { Button, Card, List, message, Modal, Result} from 'antd';
+import {Card, List, message, Result} from 'antd';
 import ReactECharts from 'echarts-for-react';
 import React, {useEffect, useState} from 'react';
 import Search from "antd/es/input/Search";
-import {deleteChartUsingPOST, listMyChartByPageUsingPOST} from "@/services/icebi/chartController";
-import {ModalForm} from "@ant-design/pro-form";
-import MyCard from "@/components/Mychart";
+import {listMyChartByPageUsingPOST} from "@/services/icebi/chartController";
+import MyModelFrom from "@/components/MyModelFrom";
 
 /**
  * 我的图表页面
@@ -19,11 +17,13 @@ const MyChartPage: React.FC = () => {
     sortOrder: 'desc',
   }
   const [searchParams, setSearchParams] = useState<API.ChartQueryRequest>({...initSearchParams});
-
   const [chartList, setChartList] = useState<API.Chart[]>();
   const [total, setTotal] = useState<number>(0);
   const [loading, setLoading] = useState<boolean>(true);
-
+  // 刷新数据的回调函数
+   const handleChartDeleted = () => {
+    loadData();
+  };
   const loadData = async () => {
     setLoading(true)
     try {
@@ -53,20 +53,6 @@ const MyChartPage: React.FC = () => {
     loadData();
   }, [searchParams])
 
-  // 删除图表
-  const handleDelete = async (id: number) => {
-    try {
-      const res = await deleteChartUsingPOST({id});
-      if (res.code !== 0) {
-        message.error('删除失败')
-      }
-      message.success('删除成功');
-      loadData(); // 刷新数据
-    } catch (error) {
-      console.error('删除图表时出错:', error);
-      message.error('删除失败');
-    }
-  };
 
   return (
     <div className='my-chart'>
@@ -110,7 +96,7 @@ const MyChartPage: React.FC = () => {
             key={item.id}
             style={{marginBottom: 10}}
           >
-            <Card style={{ width: '100%' }}>
+            <Card style={{width: '100%'}}>
               {/*图表的信息*/}
               <List.Item.Meta
                 title={item.name}
@@ -142,69 +128,11 @@ const MyChartPage: React.FC = () => {
                     <p>{'分析目标：' + item.goal}</p>
                     <div style={{marginBottom: 16}}/>
                     <div>
-                      <ReactECharts option={item.genChart && JSON.parse(item.genChart)} />
+                      <ReactECharts option={item.genChart && JSON.parse(item.genChart)}/>
                     </div>
                     <div>{item.genResult}</div>
-                  <div className='margin-16'/>
-                    <div style={{textAlign: 'right'}}>
-                      <Button danger key="delete"
-                              style={{ marginLeft: '10px', marginRight: '10px', bottom: '1' }}
-                              onClick={() => {
-                                Modal.confirm({
-                                  title: '确认删除',
-                                  content: '确定要删除这个图表吗？',
-                                  onOk: () => handleDelete(item.id ?? 0), // Convert id to a number
-                                });
-                              }}>
-                        删除
-                      </Button>
-                      <ModalForm
-                        title="详情"
-                        trigger={<Button type="primary" >详情</Button>}
-                        width="80%"
-                        submitter={{
-                          render: () => {
-                            return [
-                              <Button danger key="delete" onClick={() => {
-                                Modal.confirm({
-                                  title: '确认删除',
-                                  content: '确定要删除这个图表吗？',
-                                  onOk: () => handleDelete(item.id ?? 0), // Convert id to a number
-                                });
-                              }}>
-                                删除
-                              </Button>,
-                            ];
-                          }
-                        }}
-                        onFinish={async (values) => {
-                          console.log(values);
-                          message.success('删除成功');
-                          return true;
-                        }}
-                      >
-                        <div style={{marginBottom: 16}}/>
-                        <div>{'图表标题: ' + item.name}</div>
-                        <div>{'图表ID: ' + item.id}</div>
-                        <div>{'创建时间: ' + new Date(item.createTime).toLocaleString('zh-CN', {
-                          year: 'numeric',
-                          month: '2-digit',
-                          day: '2-digit',
-                          hour: '2-digit',
-                          minute: '2-digit',
-                          second: '2-digit',
-                        })}</div>
-                        <div>{'分析目标：' + item.goal}</div>
-                        <div style={{marginBottom: 16}}/>
-                        {/*修复bug 把ReactECharts放入Card标签中*/}
-                        {/*出现bug的原因可能是，option的加载比div的加载速度快，导致第一次加载没有挂在到div里，再次点开，才被挂载到*/}
-
-                       <MyCard item={item}/>
-
-
-                        <div>{item.genResult}</div>
-                      </ModalForm>
-                    </div>
+                    <div className='margin-16'/>
+                    <MyModelFrom item={item} onChartDeleted={handleChartDeleted}/>
                   </>
                 }
                 {
@@ -214,18 +142,7 @@ const MyChartPage: React.FC = () => {
                       title="图表生成失败"
                       subTitle={item.execMessage}
                     />
-                    <div style={{textAlign:"right"}}>
-                      <Button danger key="delete"
-                              onClick={() => {
-                                Modal.confirm({
-                                  title: '确认删除',
-                                  content: '确定要删除这个图表吗？',
-                                  onOk: () => handleDelete(item.id ?? 0), // Convert id to a number
-                                });
-                              }}>
-                        删除
-                      </Button>
-                    </div>
+                    <MyModelFrom item={item} onChartDeleted={handleChartDeleted}/>
                   </>
                 }
               </>
